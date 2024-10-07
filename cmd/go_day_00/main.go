349d963deb47701b973956c069b79da09b1b31dd
Adel Sevastianov <102406080+Saracomethstein@users.noreply.github.com>
2024-10-07 17:56:56 +0300

    up: cli and main

--- cmd/go_day_00/main.go
+++ cmd/go_day_00/main.go
@@ -1,29 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"strconv"
-
 	"github.com/Saracomethstein/go_day_00/internal/anscombe"
+	"github.com/Saracomethstein/go_day_00/internal/cli"
 )
 
 func main() {
-	var inputData string
-	dataSlice := make([]int, 0)
-	for {
-		fmt.Scan(&inputData)
-		if inputData == "!end" {
-			break
-		}
-
-		num, err := strconv.Atoi(inputData)
-		if err != nil {
-			fmt.Println("Data is not a number!")
-			continue
-		}
-		dataSlice = append(dataSlice, num)
-	}
-
+	dataSlice := cli.ScanData()
 	result, _ := anscombe.MakeCalc(anscombe.Metrics{}, dataSlice)
 	anscombe.GetInfo(result)
 }
--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -1,1 +1,39 @@
 package cli
+
+import (
+	"fmt"
+	"strconv"
+)
+
+const (
+	hello    = "Hi! Enter nums for calc metrics (write !end for break)..."
+	error    = "Slice is empty."
+	choice   = "Choice the mode:"
+	modeInfo = "Mean\t(1)\nMedian\t(2)\nMode\t(3)\nSD\t(4)\nAll\t(5)\nRestart\t(!t)\nQuit\t(!q)\n"
+)
+
+func ScanData() []int {
+	fmt.Println(hello)
+
+	var inputData string
+	dataSlice := make([]int, 0)
+	for {
+		fmt.Scan(&inputData)
+		if inputData == "!end" {
+			break
+		}
+
+		num, err := strconv.Atoi(inputData)
+		if err != nil {
+			fmt.Println("Data is not a number!")
+			continue
+		}
+		dataSlice = append(dataSlice, num)
+	}
+
+	if len(dataSlice) == 0 {
+		fmt.Println(error)
+		return nil
+	}
+	return dataSlice
+}
